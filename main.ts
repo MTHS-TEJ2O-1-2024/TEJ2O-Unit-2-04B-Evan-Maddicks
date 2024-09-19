74724c58b4d6bdad361e405a2f8f957632046bb2
Evan-Maddicks <evan.maddicks@mths.ca>
2024-09-19 11:25:49 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: Sep 2024
+ * This program will show the current temperature
 */
 
-basic.showString('Hello, World!')
+let temperature = input.temperature()
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+input.temperature()
+
+input.onButtonPressed(Button.A, function() {
+    let temperature = input.temperature()
+basic.showString('The current temperature is:')
+basic.showNumber(temperature)})
